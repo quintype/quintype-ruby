f06e4a5ef5901083862bcb045b64a6512eae6138
Dheeraj Kumar <a.dheeraj.kumar@gmail.com>
2016-03-22 10:16:31 +0530

    Fixing autoload issues.

--- lib/quintype/api/url.rb
+++ lib/quintype/api/url.rb
@@ -1,36 +1,38 @@
-class URL
-  class << self
-    def story (story)
-      "/" + story['slug']
-    end
-
-    def topic (tag_name)
-      "/topic/" + encode_uri_component(tag_name)
-    end
-
-    def section (section_name)
-      "/section/" + make_slug(section_name)
-    end
-
-    def search (term)
-      "/search?q=" + term
-    end
-
-    def author (args)
-      id = args['id']
-      slug = args['slug']
-
-      "/author/#{id}/#{slug}"
-    end
-
-    def encode_uri_component(s)
-      URI.escape(s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")) if s
-    end
-
-    private
-
-      def make_slug(s)
-        s.gsub(/[^\w -]/, "").gsub(/\s+/, "-").downcase if s
+class API
+  class URL
+    class << self
+      def story (story)
+        "/" + story['slug']
       end
+
+      def topic (tag_name)
+        "/topic/" + encode_uri_component(tag_name)
+      end
+
+      def section (section_name)
+        "/section/" + make_slug(section_name)
+      end
+
+      def search (term)
+        "/search?q=" + term
+      end
+
+      def author (args)
+        id = args['id']
+        slug = args['slug']
+
+        "/author/#{id}/#{slug}"
+      end
+
+      def encode_uri_component(s)
+        URI.escape(s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")) if s
+      end
+
+      private
+
+        def make_slug(s)
+          s.gsub(/[^\w -]/, "").gsub(/\s+/, "-").downcase if s
+        end
+    end
   end
 end
